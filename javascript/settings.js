74836e8606919fa033a1f5ab2d27df4f7207f514
Daniele63 <battle1000@hotmail.com>
2025-01-12 14:52:03 +0100

    Added background music when sound is on

--- javascript/settings.js
+++ javascript/settings.js
@@ -5,14 +5,12 @@ const TEXT_LARGE_BUTTON = document.getElementById("large");
 const SOUND_ON_BUTTON = document.getElementById("on");
 const SOUND_OFF_BUTTON = document.getElementById("off");
 
-const MAIN_CONTENT = document.getElementById("mainContent");
-
 // Small Text
 TEXT_SMALL_BUTTON.addEventListener("click", function () {
   TEXT_SMALL_BUTTON.setAttribute("class", "settings-buttons-active");
   TEXT_MEDIUM_BUTTON.setAttribute("class", "settings-buttons");
   TEXT_LARGE_BUTTON.setAttribute("class", "settings-buttons");
-  MAIN_CONTENT.style.fontSize = "0.9em";
+  document.body.style.fontSize = "0.9em";
 });
 
 // Medium Text
@@ -20,7 +18,7 @@ TEXT_MEDIUM_BUTTON.addEventListener("click", function () {
   TEXT_SMALL_BUTTON.setAttribute("class", "settings-buttons");
   TEXT_MEDIUM_BUTTON.setAttribute("class", "settings-buttons-active");
   TEXT_LARGE_BUTTON.setAttribute("class", "settings-buttons");
-  MAIN_CONTENT.style.fontSize = "1em";
+  document.body.style.fontSize = "1em";
 });
 
 // Large Text
@@ -28,17 +26,28 @@ TEXT_LARGE_BUTTON.addEventListener("click", function () {
   TEXT_SMALL_BUTTON.setAttribute("class", "settings-buttons");
   TEXT_MEDIUM_BUTTON.setAttribute("class", "settings-buttons");
   TEXT_LARGE_BUTTON.setAttribute("class", "settings-buttons-active");
-  MAIN_CONTENT.style.fontSize = "1.1em";
+  document.body.style.fontSize = "1.1em";
 });
+
+let background_music = new Audio("../sounds/background_music.mp3");
+let sound_on = false;
 
 // Sound On
 SOUND_ON_BUTTON.addEventListener("click", function () {
   SOUND_ON_BUTTON.setAttribute("class", "settings-buttons-active");
   SOUND_OFF_BUTTON.setAttribute("class", "settings-buttons");
+  // Play Bg music
+  background_music.play();
+  // Sound on variable
+  sound_on = true;
 });
 
 // Sound Off
 SOUND_OFF_BUTTON.addEventListener("click", function () {
   SOUND_ON_BUTTON.setAttribute("class", "settings-buttons");
   SOUND_OFF_BUTTON.setAttribute("class", "settings-buttons-active");
+  // Stop Bg music
+  background_music.pause();
+  // Sound on variable
+  sound_on = false;
 });
