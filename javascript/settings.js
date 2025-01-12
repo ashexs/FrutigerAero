f18802e4ebbd908242ed016da4d88d316a9c07d8
Daniele63 <battle1000@hotmail.com>
2025-01-12 05:20:00 +0100

    Change text size buttons

---
+++ javascript/settings.js
@@ -0,0 +1,44 @@
+// Buttons
+const TEXT_SMALL_BUTTON = document.getElementById("small");
+const TEXT_MEDIUM_BUTTON = document.getElementById("medium");
+const TEXT_LARGE_BUTTON = document.getElementById("large");
+const SOUND_ON_BUTTON = document.getElementById("on");
+const SOUND_OFF_BUTTON = document.getElementById("off");
+
+const MAIN_CONTENT = document.getElementById("mainContent");
+
+// Small Text
+TEXT_SMALL_BUTTON.addEventListener("click", function () {
+  TEXT_SMALL_BUTTON.setAttribute("class", "settings-buttons-active");
+  TEXT_MEDIUM_BUTTON.setAttribute("class", "settings-buttons");
+  TEXT_LARGE_BUTTON.setAttribute("class", "settings-buttons");
+  MAIN_CONTENT.style.fontSize = "0.9em";
+});
+
+// Medium Text
+TEXT_MEDIUM_BUTTON.addEventListener("click", function () {
+  TEXT_SMALL_BUTTON.setAttribute("class", "settings-buttons");
+  TEXT_MEDIUM_BUTTON.setAttribute("class", "settings-buttons-active");
+  TEXT_LARGE_BUTTON.setAttribute("class", "settings-buttons");
+  MAIN_CONTENT.style.fontSize = "1em";
+});
+
+// Large Text
+TEXT_LARGE_BUTTON.addEventListener("click", function () {
+  TEXT_SMALL_BUTTON.setAttribute("class", "settings-buttons");
+  TEXT_MEDIUM_BUTTON.setAttribute("class", "settings-buttons");
+  TEXT_LARGE_BUTTON.setAttribute("class", "settings-buttons-active");
+  MAIN_CONTENT.style.fontSize = "1.1em";
+});
+
+// Sound On
+SOUND_ON_BUTTON.addEventListener("click", function () {
+  SOUND_ON_BUTTON.setAttribute("class", "settings-buttons-active");
+  SOUND_OFF_BUTTON.setAttribute("class", "settings-buttons");
+});
+
+// Sound Off
+SOUND_OFF_BUTTON.addEventListener("click", function () {
+  SOUND_ON_BUTTON.setAttribute("class", "settings-buttons");
+  SOUND_OFF_BUTTON.setAttribute("class", "settings-buttons-active");
+});
